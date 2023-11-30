921688f12bda20e61409cb35ab488cf24b446341
vikovaro <98817333+vikovaro@users.noreply.github.com>
2023-11-30 14:28:26 +0300

    temp

---
+++ DM3 v1/DM3/MainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Дата: 21.10.2019
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+namespace DM3
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox tz;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+
+		{
+			this.tz = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tz
+			// 
+			this.tz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.tz.Location = new System.Drawing.Point(24, 24);
+			this.tz.Name = "tz";
+			this.tz.Size = new System.Drawing.Size(86, 26);
+			this.tz.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(24, 58);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(86, 26);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "ВВОД";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 99);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(280, 244);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(316, 58);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(263, 273);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(149, 58);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(112, 27);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "По умолчанию";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(132, 24);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(86, 25);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Вывод";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.AllowUserToAddRows = false;
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(608, 56);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.Size = new System.Drawing.Size(244, 273);
+			this.dataGridView2.TabIndex = 6;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(224, 24);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(74, 25);
+			this.button4.TabIndex = 7;
+			this.button4.Text = "Очистить";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(375, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(204, 19);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Матрица инцидентности";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(669, 32);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(183, 18);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Матрица смежности";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(864, 355);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.dataGridView2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.tz);
+			this.Name = "MainForm";
+			this.Text = "DM3";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+}
+}
---
+++ DM3 v1/DM3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Дата: 21.10.2019
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+using System.Windows.Forms;
+
+namespace DM3
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
